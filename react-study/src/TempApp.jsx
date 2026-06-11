0d37d179ea8fbd18f8009a9e28fae48286dfad74
ennikyoungeun <ennikyoungeun@gmail.com>
2026-06-11 12:00:00 +0900

    리액트 시작 프로젝트 생성및 실행

---
+++ react-study/src/TempApp.jsx
@@ -0,0 +1,13 @@
+import './TempApp.css';
+
+function TempApp(){
+
+    return(
+        <div>
+            <h1>TempApp Component</h1>
+            <h2>CSS TEST</h2>
+        </div>
+    )
+}
+
+export default TempApp;

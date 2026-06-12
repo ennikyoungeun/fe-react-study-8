026fc6dd0d2711dd7408816bb8a78f0602eb4159
ennikyoungeun <ennikyoungeun@gmail.com>
2026-06-12 15:04:57 +0900

    배열 활용 유용한 함수. react데이터 기반 반복처리

--- react-study/src/App.jsx
+++ react-study/src/App.jsx
@@ -8,6 +8,7 @@ import State01 from './components/State01'
 import Export01 from './components/Export01'
 
 import Quiz01 from './quiz/quiz01/quiz01'
+import State02 from './components/State02'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,7 +21,9 @@ function App() {
 
     //<Export01/>
 
-    <Quiz01/>
+    //<Quiz01/>
+
+    <State02/>
    )
 }
 
